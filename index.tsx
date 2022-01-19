ed5fa6ed338002c5159ef5708abaf95c57622cc5
BEN EL HADI <95426950+leviathan2369@users.noreply.github.com>
2022-01-19 14:26:40 +0100

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./redux/Store";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
